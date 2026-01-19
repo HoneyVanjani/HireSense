1d3b52665c54241885daadf297137bf2a5dfcbf7
HoneyVanjani <honeyvanjani43@gmail.com>
2026-01-19 20:49:11 +0530

    Added User file

--- src/main/java/com/hiresense/auth/model/User.java
+++ src/main/java/com/hiresense/auth/model/User.java
@@ -1,25 +1,10 @@
 package com.hiresense.auth.model;
 
-import jakarta.persistence.*;
+import lombok.Data;
 
-@Entity
-@Table(name = "users")
+@Data
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false)
     private String password;
-
-    @Column(nullable = false)
-    private boolean active;
-
-    protected User() {
-        // JPA requires a default constructor
-    }
 }
